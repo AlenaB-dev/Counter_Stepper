e880d958d43b59fd44c402c992a0ab416473f0bb
Alena Petrych <abelokonskaya.87@gmail.com>
2025-11-05 10:06:32 +0000

    style: add styles to the Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import styles from "./Counter.module.css";
 
 function Counter() {
-  const count = useState();
-  return;
+  const { count, setCounter } = useState();
+  return (
+    <>
+      <h1>Counter and Stepper App</h1>
+      <div className={styles.counter}>
+        <button className={styles.btn}>-</button>
+        <button className={styles.btn}>+</button>
+        <input placeholder="Inter your step"></input>
+      </div>
+    </>
+  );
 }
 
 export default Counter;
